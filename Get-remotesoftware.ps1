58fc15dcc1b4753a50df0e17991e0925806de9e1
kenvb <kenvdb@gmail.com>
2016-03-13 22:27:21 +0100

    1st commit

---
+++ Get-remotesoftware.ps1
@@ -0,0 +1,20 @@
+﻿#This script gets all the Computers in the AD and then gets all the software installed on those computers.
+$ADcomputer = get-adcomputer -Filter * 
+$result = ForEach ($computer in $ADcomputer){
+
+$Pingtest= Test-NetConnection $computer.Name
+    if($Pingtest.Pingsucceeded -eq $true)
+    {
+    write-host $computer.name
+    write-host $pingtest.PingSucceeded
+    invoke-command -ComputerName $computer.name -ScriptBlock{
+    Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* <# | Where-Object {$_.Publisher -eq "Microsoft Corporation"} #> | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+    }
+    }
+    else
+    {
+    write-host $computer.name
+    write-host $pingtest.PingSucceeded
+    }
+}
+$result | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate,PSComputerName | Export-Csv C:\drivers\export.csv
