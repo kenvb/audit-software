5b66c4c6c3bbb275df1a9b5c7f156b73ef508ce5
kenvb <kenvdb@gmail.com>
2016-03-14 21:52:07 +0100

    Parameter cleanup

--- Get-remotesoftware.ps1
+++ Get-remotesoftware.ps1
@@ -1,15 +1,10 @@
 ﻿<#
+#Requires -Version 4.0
 .SYNOPSIS
 A simple script to check for installed software on computers in an AD environment. Results will be exported to a csv-file, offline computers to a txt-file.
 Offline computers are determined by a connection test to the WINRM port. Beware of the firewall.
 .DESCRIPTION
 A simple script to check for installed software on computers in an AD environment.
-.PARAMETER CSVLocation
-location of your .csv with usernames (string)
-.PARAMETER password
-Provide a password for all your users in the .csv file (string)
-.PARAMETER IsDisabled 
-Is a switch and doesn't require input. Disables the user account on creation (or more correctly: doesn't enable it) 
 .EXAMPLE
 ./get-remotesoftware.ps1
 Runs the script and performs the tasks as explained in the SYNOPSIS
